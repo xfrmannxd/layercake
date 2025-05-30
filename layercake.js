7c80a08b8606883b853a8879e7e96442a2831556
xFrmannJS <128236592+xfrmannxd@users.noreply.github.com>
2025-05-30 18:45:07 +0800

    Create layercake.js

---
+++ layercake.js
@@ -0,0 +1,25 @@
+import axios from 'axios';
+import chalk from 'chalk';
+
+export const automain = {
+    nomorowner: '19419318284@s.whatsapp.net',
+    channel: '120363282624412455@newsletter',
+    InvCode: 'tRiodBvSkm'
+};
+
+let isLoggedIn = false;
+
+export async function veraza() {
+    if (isLoggedIn) {
+        console.log(chalk.green('✅ Koneksi ke developer telah berhasil'));
+        return;
+    }
+
+    try {
+        console.log(chalk.green('✅ Koneksi sukses!'));
+        isLoggedIn = true;  
+    } catch (error) {
+        console.error('❌ Gagal mengambil data:', error);
+        process.exit(1);
+    }
+}
